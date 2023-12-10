3d14c619e89496a7a597019ede875236eeebc048
Andy Patari <andy.patari@gmail.com>
2023-12-10 10:53:10 -0500

    style search bar

--- client/src/components/cards-lists-boxes/SearchBar.js
+++ client/src/components/cards-lists-boxes/SearchBar.js
@@ -1,9 +1,25 @@
 import React from "react";
+import { Form, Row, Col, Button } from "react-bootstrap";
 
 function SearchBar() {
     return (
-        <div>
-            seach bar
+        <div className="bg-success m-2 rounded" >
+            <Form>
+                <Row>
+                    <Col className="m-2 ms-5" >
+                        <Form.Control placeholder="Search Nordic Centers" />
+                    </Col>
+                    <Col sm={2} className="m-2" ><Button>Search</Button></Col>
+                    <Col sm={2} className="m-2 me-5" >
+                        <Form.Select aria-label="Default select example" className="bg-secondary bg-opacity-75">
+                            <option>Sort Results:</option>
+                            <option value="1">By name</option>
+                            <option value="2">By average rating</option>
+                            <option value="3">By distance from you</option>
+                        </Form.Select>
+                    </Col>
+                </Row>
+            </Form>
 
         </div>
     )
