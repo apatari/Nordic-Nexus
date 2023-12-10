f13f5e6266d556f3cbf5db79f98db430777bac97
Andy Patari <andy.patari@gmail.com>
2023-12-10 10:05:03 -0500

    create detail and search component placeholders

--- client/src/components/NordicCenters.js
+++ client/src/components/NordicCenters.js
@@ -2,34 +2,12 @@ import React, { useState, useEffect } from "react";
 
 function NordicCenters () {
 
-    const [weather, setWeather] = useState(null)
-
-    const fetchLink = `http://api.openweathermap.org/data/3.0/onecall?` + 
-    `lat=42.85&lon=-72.56&units=imperial&exclude=minutely,hourly` + 
-    `&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-
-
-    useEffect(() => {
-        fetch(fetchLink)
-        .then(res => res.json())
-        .then(data => 
-            {setWeather(data)
-            console.log(data)})
-       
-    }, [])
+    
 
     return (
         <div>
             <h2>nordic centers</h2>
-            <h4>Weather report:</h4>
-            <div className="m-3" >
-                {weather? 
-                weather.daily.map(day=> {
-                    return <p key={day.dt} > 
-                        <strong>{Math.round(day.temp.max)} / {Math.round(day.temp.min)} : </strong> 
-                        Short: {day.weather[0].description} Long: {day.summary} </p>
-                }) : "Loading..."}
-            </div>
+            
         </div>
     )
 }
---
+++ client/src/components/cards-lists-boxes/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function SearchBar() {
+    return (
+        <div>
+            seach bar
+
+        </div>
+    )
+}
+
+export default SearchBar
