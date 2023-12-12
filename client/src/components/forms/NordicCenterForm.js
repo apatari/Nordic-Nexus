160f3ba25b1121c57e3c3602bcb1e10e9dbcb782
Andy Patari <andy.patari@gmail.com>
2023-12-11 19:49:48 -0500

    formik and autocomplete for nordic center form

--- client/src/components/forms/NordicCenterForm.js
+++ client/src/components/forms/NordicCenterForm.js
@@ -1,14 +1,53 @@
-import React from "react";
+import React, { useRef } from "react";
 import { Row, Col, Form, Button } from "react-bootstrap";
+import { useFormik } from "formik";
+import * as yup from "yup"
+import { useJsApiLoader, Autocomplete } from '@react-google-maps/api'
+
+const lib = ['places']
 
 function NordicCenterForm() {
+
+    const addressRef = useRef()
+
+    const {isLoaded} = useJsApiLoader({
+        libraries: lib,
+        googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_KEY
+      })
+
+    const formSchema = yup.object().shape({
+        name: yup.string().max(100, "Name must be 100 characters or fewer").required("Please enter a name"),
+        latitude: yup.number().required("Enter a latitude").max(180, "Must be less than 180 degrees").min(-180, "must be greater than -180 degrees"),
+        longitude: yup.number().required("Enter a longitude").max(180, "Must be less than 180 degrees").min(-180, "must be greater than -180 degrees"),
+        report_url: yup.string(),
+        map_url: yup.string()
+    })
+
+    const formik = useFormik({
+        initialValues: {
+            name: "",
+            latitude: "",
+            longitude: "",
+            report_url: "",
+            map_url: "",
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {
+            console.log({...values, "address": addressRef.current.value})
+        }
+    })
+
+    if (!isLoaded) {
+        return <h2 className='m-3' >loading</h2>
+      }
+
     return (
         <div className="ms-4" >
            
                 
-            <Col lg={5} className="m-5" >
+            <Col lg={6} className="m-5" >
                 <h3>New Nordic Center</h3>
-                <Form  >
+                <Form onSubmit={formik.handleSubmit} >
                     
                     <Form.Group className="form-floating m-3"  >
                         <Form.Control 
@@ -16,38 +55,40 @@ function NordicCenterForm() {
                             id="name" 
                             name="name"
                             placeholder="Name"
-                            // value={formik.values.name}
-                            // onChange={formik.handleChange}
+                            value={formik.values.name}
+                            onChange={formik.handleChange}
                             />
 
                         <Form.Label>Name</Form.Label>
-                        {/* {formik.errors.name ? <div className="text-danger" >{formik.errors.name}</div> : ""} */}
+                        {formik.errors.name ? <div className="text-danger" >{formik.errors.name}</div> : ""}
                     </Form.Group>
-                    <Form.Group className="form-floating m-3"  >
+                    <Autocomplete>
+                    <Form.Group className="form-floating mt-3 mx-3"  >
                         <Form.Control 
                             type="text" 
                             id="address" 
                             name="address"
                             placeholder="Address"
-                            // value={formik.values.address}
-                            // onChange={formik.handleChange}
+                            ref={addressRef}
                             />
 
                         <Form.Label>Address</Form.Label>
                         {/* {formik.errors.address ? <div className="text-danger" >{formik.errors.address}</div> : ""} */}
-                    </Form.Group>
+                    </Form.Group></Autocomplete>
                     <Row>
                         <Col>
                             <Form.Group className="form-floating m-3 me-1"  >
                                 <Form.Control 
-                                    type="text" 
+                                    type="number" 
                                     id="latitude" 
                                     name="latitude"
                                     placeholder="Latitude"
-                                    // value={formik.values.latitude}
-                                    // onChange={formik.handleChange}
+                                    step={'any'}
+                                    value={formik.values.latitude}
+                                    onChange={formik.handleChange}
                                     />
 
+                                {formik.errors.latitude ? <div className="text-danger" >{formik.errors.latitude}</div> : ""}
                                 <Form.Label>Latitude</Form.Label>
                                 
                             </Form.Group>
@@ -55,34 +96,35 @@ function NordicCenterForm() {
                         <Col> 
                             <Form.Group className="form-floating m-3 ms-1"  >
                                 <Form.Control 
-                                    type="text" 
+                                    type="number" 
                                     id="longitude" 
                                     name="longitude"
                                     placeholder="Longitudes"
-                                    // value={formik.values.longitude}
-                                    // onChange={formik.handleChange}
+                                    step={'any'}
+                                    value={formik.values.longitude}
+                                    onChange={formik.handleChange}
                                     />
 
+                                {formik.errors.longitude ? 
+                                    <div className="text-danger" >{formik.errors.longitude}</div> : ""}
                                 <Form.Label>Longitude</Form.Label>
                             </Form.Group>
                         </Col>
-                        {/* {formik.errors.longitude ? <div className="text-danger" >{formik.errors.longitude}</div> : ""} */}
-                        {/* {formik.errors.latitude ? <div className="text-danger" >{formik.errors.latitude}</div> : ""} */}
                     </Row>
 
                     
-                    <Form.Group className="form-floating m-3"  >
+                    <Form.Group className="form-floating mb-3 mx-3"  >
                         <Form.Control 
                             type="text" 
                             id="report_url" 
                             name="report_url"
                             placeholder="Trail Report URL"
-                            // value={formik.values.report_url}
-                            // onChange={formik.report_url}
+                            value={formik.values.report_url}
+                            onChange={formik.handleChange}
                             />
 
                         <Form.Label>Trail Report URL (optional)</Form.Label>
-                        {/* {formik.errors.report_url ? <div className="text-danger" >{formik.errors.report_url}</div> : ""} */}
+                        {formik.errors.report_url ? <div className="text-danger" >{formik.errors.report_url}</div> : ""}
                     </Form.Group>
 
                     <Form.Group className="form-floating m-3"  >
@@ -91,12 +133,12 @@ function NordicCenterForm() {
                             id="map_url" 
                             name="map_url"
                             placeholder="Trail Map URL"
-                            // value={formik.values.map_url}
-                            // onChange={formik.map_url}
+                            value={formik.values.map_url}
+                            onChange={formik.handleChange}
                             />
 
                         <Form.Label>Trail Map URL (optional)</Form.Label>
-                        {/* {formik.errors.map_url ? <div className="text-danger" >{formik.errors.map_url}</div> : ""} */}
+                        {formik.errors.map_url ? <div className="text-danger" >{formik.errors.map_url}</div> : ""}
                     </Form.Group>
                     <div className="d-flex" >
                         <Button type="submit" size='lg' className="ms-auto me-3 btn-success " >Submit</Button>
