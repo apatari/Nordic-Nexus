6ffa1dc3ffbdbe3f9e3f6aed0616d47bc38fd041
Andy Patari <andy.patari@gmail.com>
2023-12-14 13:23:48 -0500

    formik controls started for newTrip

--- client/src/components/forms/TripForm.js
+++ client/src/components/forms/TripForm.js
@@ -1,9 +1,17 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useContext } from "react";
 import { Col, Row, Form, Button } from "react-bootstrap";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import { UserContext } from "../App";
+import { useFormik } from "formik";
+import * as yup from "yup"
 
 function TripForm() {
 
     const [nordicCenters, setNordicCenters] = useState(null)
+
+    const [user] = useContext(UserContext)
+
+    const history = useHistory()
     
     useEffect(() => {
         fetch('/api/nordiccenters')
@@ -11,26 +19,72 @@ function TripForm() {
         .then(data => setNordicCenters(data))
     }, [])
 
+    const formSchema = yup.object().shape({
+        comment: yup.string().max(200, "Must be fewer than 200 characters"),
+        nordic_center_id: yup.number().integer().required("Must select a Nordic center"),
+        snow_cover: yup 
+          .number()
+          .integer()
+          .required("Must enter a rating")
+          .typeError("Please enter an Integer")
+          .max(5)
+          .min(0),
+        grooming: yup 
+          .number()
+          .integer()
+          .required("Must enter a rating")
+          .typeError("Please enter an Integer")
+          .max(5)
+          .min(0),
+        weather: yup 
+          .number()
+          .integer()
+          .required("Must enter a rating")
+          .typeError("Please enter an Integer")
+          .max(5)
+          .min(0),
+        fun_factor: yup 
+          .number()
+          .integer()
+          .required("Must enter a rating")
+          .typeError("Please enter an Integer")
+          .max(5)
+          .min(0),
+      });
 
+    const formik = useFormik({
+        initialValues: {
+            comment: "",
+            nordic_center_id: "",
+            snow_cover: 0,
+            grooming: 0,
+            weather: 0,
+            fun_factor: 0,
 
+        },
+        validationSchema: formSchema,
+        onSubmit: (values) => {console.log({...values, "user_id": user.id})}
+    })
 
     return (
         <div className="ms-4" >
             <Col lg={8} className="m-5 p-3 bg-success bg-opacity-25 rounded-4" >
                 <h3 className="text-primary" >New Trip</h3>
-                <Form className="my-3" >
+                <Form className="my-3" onSubmit={formik.handleSubmit} >
                     <Row className="mx-2" >
                         <Col>
-                            <Form.Select  aria-label="Default select example" className="bg-light bg-opacity-75">
+                            <Form.Select  
+                                aria-label="Default select example" 
+                                id="nordic_center_id" 
+                                name="nordic_center_id" 
+                                onChange={formik.handleChange} 
+                                value={formik.values.nordic_center_id}
+                                className="bg-light bg-opacity-75">
                                 <option>Nordic Center:</option>
                                 {nordicCenters? nordicCenters.map(center =>{
                                     return <option key={center.id} value={center.id} > {center.name}</option>
                                 }): ""  }
 
-                                {/* <option value="name">Name A-Z</option>
-                                <option value="nameDesc">Name Z-A</option>
-                                <option value="rating">Average rating</option> */}
-                                
                             </Form.Select>
                         </Col>
                         <Col>
@@ -48,11 +102,11 @@ function TripForm() {
                                             inline
                                             value={num}
                                             label={num}
-                                            name="rating"
+                                            name="snow_cover"
                                             type='radio'
                                             id={`inline-radio-${num}`}
-                                            // checked={formik.values.rating === num}
-                                            // onChange={formik.handleChange}   
+                                            checked={formik.values.snow_cover == num}
+                                            onChange={formik.handleChange}   
                                         />
                                         )
                                     })
@@ -71,11 +125,11 @@ function TripForm() {
                                             inline
                                             value={num}
                                             label={num}
-                                            name="rating"
+                                            name="grooming"
                                             type='radio'
                                             id={`inline-radio-${num}`}
-                                            // checked={formik.values.rating === num}
-                                            // onChange={formik.handleChange}   
+                                            checked={formik.values.grooming == num}
+                                            onChange={formik.handleChange}   
                                         />
                                         )
                                     })
@@ -94,11 +148,11 @@ function TripForm() {
                                             inline
                                             value={num}
                                             label={num}
-                                            name="rating"
+                                            name="weather"
                                             type='radio'
                                             id={`inline-radio-${num}`}
-                                            // checked={formik.values.rating === num}
-                                            // onChange={formik.handleChange}   
+                                            checked={formik.values.weather == num}
+                                            onChange={formik.handleChange}   
                                         />
                                         )
                                     })
@@ -117,11 +171,11 @@ function TripForm() {
                                             inline
                                             value={num}
                                             label={num}
-                                            name="rating"
+                                            name="fun_factor"
                                             type='radio'
                                             id={`inline-radio-${num}`}
-                                            // checked={formik.values.rating === num}
-                                            // onChange={formik.handleChange}   
+                                            checked={formik.values.fun_factor == num}
+                                            onChange={formik.handleChange}   
                                         />
                                         )
                                     })
@@ -137,8 +191,8 @@ function TripForm() {
                             name="comment"
                             style={{ height: '100px'}}
                             placeholder="Comment(optional)"
-                            // value={formik.values.name}
-                            // onChange={formik.handleChange}
+                            value={formik.values.comment}
+                            onChange={formik.handleChange}
                             />
 
                         <Form.Label>Comment (optional)</Form.Label>
