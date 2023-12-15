be55d02ccf581a9eed8a8a95b05f95de64b36299
Andy Patari <andy.patari@gmail.com>
2023-12-15 08:55:39 -0500

    add tripCard placeholders

--- client/src/components/cards-lists-boxes/TripList.js
+++ client/src/components/cards-lists-boxes/TripList.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from "react";
 
 import { Row } from "react-bootstrap";
 import TripCard from "./TripCard";
+import TripCardPlaceholder from "./TripCardPlaceholder"
 
 
 function TripList( {center} ) {
@@ -17,6 +18,24 @@ function TripList( {center} ) {
     }, [])
 
 
+    if (!trips) {
+        return (
+            <div className="me-2 mt-4" >
+            <Row>
+                <h4 className="m-2" >Latest Trips</h4>
+            </Row>
+            <div  className="d-flex flex-wrap">
+            <Row>
+                <TripCardPlaceholder />
+                <TripCardPlaceholder />
+                <TripCardPlaceholder />
+                
+            </Row>
+            </div>
+        
+        </div>
+        )
+    }
 
     return (
         <div className="me-2 mt-4" >
@@ -32,7 +51,7 @@ function TripList( {center} ) {
                 trips.filter(trip => trip.nordic_center.id === parseInt(center)).slice(0,TRIP_COUNT).map(trip => {
                     return <TripCard trip={trip}key={trip.id} />
                 })
-            }
+                }
                 
             </Row>
             </div>
