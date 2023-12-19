e858fd07d8f5ecedd7cce403b2e5e8603e703b5c
Andy Patari <andy.patari@gmail.com>
2023-12-19 13:17:24 -0500

    cache weather 10 mins

--- client/src/components/NordicCenterDetail.js
+++ client/src/components/NordicCenterDetail.js
@@ -11,6 +11,8 @@ import InfoAndMapContainer from "./InfoAndMapContainer";
 
 function NordicCenterDetail() {
 
+    const CACHE_DURATION_MINUTES = 10
+
     const [weather, setWeather] = useState(null)
     const [nordicCenter, setNordicCenter] =  useState(null)
 
@@ -21,13 +23,27 @@ function NordicCenterDetail() {
         .then(res => res.json())
         .then(data => {
             setNordicCenter(data)
-            const weatherFetchLink = `https://api.openweathermap.org/data/3.0/onecall?` + 
-                `lat=${data.latitude}&lon=${data.longitude}&units=imperial&exclude=minutely,hourly` + 
-                `&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
-            fetch(weatherFetchLink)
-            .then(res => res.json())
-            .then(data => setWeather(data))
-        
+
+            const cachedWeather = JSON.parse(localStorage.getItem(nordic_center_id))
+            const now = new Date().getTime()
+
+            if (!cachedWeather || ((now - cachedWeather.time) > (60000 * CACHE_DURATION_MINUTES))) {
+                    
+                const weatherFetchLink = `https://api.openweathermap.org/data/3.0/onecall?` + 
+                    `lat=${data.latitude}&lon=${data.longitude}&units=imperial&exclude=minutely,hourly` + 
+                    `&appid=${process.env.REACT_APP_WEATHER_API_KEY}`
+                fetch(weatherFetchLink)
+                .then(res => res.json())
+                .then(data => {
+                    setWeather(data)
+                    const newNow = new Date()
+                    const weatherObject = {"weather": data, "time": newNow.getTime()}
+                    localStorage.setItem(nordic_center_id, JSON.stringify(weatherObject) )
+                })
+            } else {
+                
+                setWeather(cachedWeather.weather)
+            }
         })
             
         }
