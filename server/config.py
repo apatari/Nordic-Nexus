df88b9c4adfac3426542e92a230f14d0183b21b5
Andy Patari <andy.patari@gmail.com>
2023-12-19 09:15:03 -0500

    config swap

--- server/config.py
+++ server/config.py
@@ -22,8 +22,8 @@ app = Flask(
     )
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI')
 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.json.compact = False
